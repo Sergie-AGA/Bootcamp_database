9fbfe2898e6d04a7c9d925cfd547f13d59c6210d
Sergie <sergiearruda@gmail.com>
2021-02-12 19:17:06 +0000

    Handles JWT and cookie

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -32,7 +32,7 @@ const BootcampSchema = new mongoose.Schema(
       type: String,
       match: [
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please adad a valid email",
+        "Please add a valid email",
       ],
     },
     address: {
